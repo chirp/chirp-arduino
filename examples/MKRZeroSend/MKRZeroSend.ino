2c87456281c77800851d2a64e9e35203ee41c95b
Joe Todd <joe@chirp.io>
2019-08-07 09:41:26 +0100

    Restructure using arduino example best practices

--- examples/MKRZeroSend/MKRZeroSend.ino
+++ examples/MKRZeroSend/MKRZeroSend.ino
@@ -1,8 +1,8 @@
 /**-----------------------------------------------------------------------------
 
-    Example code using MKRZERO and UDA1334 audio output
+    Example code using MKRZero and UDA1334 audio output
 
-    @file cortex-m0plus.ino
+    @file MKRZeroSend.ino
 
     @brief Create a developer account at https://developers.chirp.io,
     and copy and paste your key, secret and config string for chosen
@@ -16,7 +16,17 @@
     audio data.
 
     *Note*: This board is send-only, so cannot be configured to receive
-    data due to the minimal memory available on the board.
+    data due to the minimal memory available on the board
+
+    Circuit:
+      - Arduino MKRZero
+      - Adafruit UDA1334
+
+    Alternative Boards:
+      - Arduino Vidor 4000
+      - Genuino Zero
+      - Arduino MKR Fox 1200
+      - Arduino MKR1000 WiFi
 
     Copyright © 2011-2019, Asio Ltd.
     All rights reserved.
@@ -48,112 +58,14 @@ DmacDescriptor  *desc;
 static chirp_connect_t *chirp = NULL;
 static volatile bool dma_complete = true;
 
+// Function definitions ---------------------------------------
+
+void dmaCallback(Adafruit_ZeroDMA *dma);
+void dmaErrorCallback(Adafruit_ZeroDMA *dma);
+void setupDMA(void);
+void setupChirp(void);
+
 // Function declarations --------------------------------------
-
-void dmaCallback(Adafruit_ZeroDMA *dma)
-{
-  current++;
-  if (current >= NUM_BUFFERS) {
-    current -= NUM_BUFFERS;
-  }
-  dma->changeDescriptor(desc, buffer[current]);
-  ZeroDMAstatus stat = dma->startJob();
-  if (stat == DMA_STATUS_OK) {
-    dma_complete = true;
-  } else {
-    Serial.println("ERROR");
-  }
-}
-
-void dmaErrorCallback(Adafruit_ZeroDMA *dma)
-{
-  Serial.println("DMAERROR");
-}
-
-void chirpErrorHandler(chirp_connect_error_code_t code)
-{
-  if (code != CHIRP_CONNECT_OK)
-  {
-    const char *error_string = chirp_connect_error_code_to_string(code);
-    Serial.println(error_string);
-    chirp_connect_free((void *) error_string);
-    exit(42);
-  }
-}
-
-void onSendingCallback(void *chirp, uint8_t *payload, size_t length, uint8_t channel)
-{
-  Serial.println("Sending data...");
-}
-
-void onSentCallback(void *chirp, uint8_t *payload, size_t length, uint8_t channel)
-{
-  Serial.println("Sent data...");
-}
-
-void setupChirp(void)
-{
-  chirp = new_chirp_connect(CHIRP_APP_KEY, CHIRP_APP_SECRET);
-  if (chirp == NULL)
-  {
-    Serial.println("Chirp initialisation failed.");
-    return;
-  }
-
-  chirp_connect_error_code_t err = chirp_connect_set_config(chirp, CHIRP_APP_CONFIG);
-  if (err != CHIRP_CONNECT_OK)
-    chirpErrorHandler(err);
-
-//  chirp_connect_callback_set_t callback_set = {
-//    .on_state_changed = NULL,
-//    .on_sending = onSendingCallback,
-//    .on_sent = onSentCallback,
-//    .on_receiving = NULL,
-//    .on_received = NULL
-//  };
-//
-//  err = chirp_connect_set_callbacks(chirp, callback_set);
-//  if (err != CHIRP_CONNECT_OK)
-//    chirpErrorHandler(err);
-
-  err = chirp_connect_set_output_sample_rate(chirp, SAMPLE_RATE);
-  if (err != CHIRP_CONNECT_OK)
-    chirpErrorHandler(err);
-
-  err = chirp_connect_start(chirp);
-  if (err != CHIRP_CONNECT_OK)
-    chirpErrorHandler(err);
-
-  Serial.println("Chirp SDK initialised.");
-  Serial.flush();
-}
-
-void setupDMA(void)
-{
-  Serial.println("Configuring DMA trigger");
-  dma.setTrigger(I2S_DMAC_ID_TX_0);
-  dma.setAction(DMA_TRIGGER_ACTON_BEAT);
-
-  Serial.println("Allocating DMA channel");
-  ZeroDMAstatus stat = dma.allocate();
-  dma.printStatus(stat);
-
-  Serial.println("Setting up transfer");
-  desc = dma.addDescriptor(
-    buffer[current],
-    (void *)(&I2S->DATA[0].reg),
-    BUFFER_SIZE,
-    DMA_BEAT_SIZE_WORD,
-    true,
-    false
-  );
-
-  Serial.println("Adding DMA callbacks");
-  dma.setCallback(dmaCallback);
-  dma.setCallback(dmaErrorCallback, DMA_CALLBACK_TRANSFER_ERROR);
-}
-
-// Main -------------------------------------------------------
 
 void setup()
 {
@@ -206,4 +118,111 @@ void loop()
 
     dma_complete = false;
   }
+}
+
+// Chirp -----------------------------------------------------
+
+void chirpErrorHandler(chirp_connect_error_code_t code)
+{
+  if (code != CHIRP_CONNECT_OK)
+  {
+    const char *error_string = chirp_connect_error_code_to_string(code);
+    Serial.println(error_string);
+    chirp_connect_free((void *) error_string);
+    exit(42);
+  }
+}
+
+void onSendingCallback(void *chirp, uint8_t *payload, size_t length, uint8_t channel)
+{
+  Serial.println("Sending data...");
+}
+
+void onSentCallback(void *chirp, uint8_t *payload, size_t length, uint8_t channel)
+{
+  Serial.println("Sent data...");
+}
+
+void setupChirp(void)
+{
+  chirp = new_chirp_connect(CHIRP_APP_KEY, CHIRP_APP_SECRET);
+  if (chirp == NULL)
+  {
+    Serial.println("Chirp initialisation failed.");
+    return;
+  }
+
+  chirp_connect_error_code_t err = chirp_connect_set_config(chirp, CHIRP_APP_CONFIG);
+  if (err != CHIRP_CONNECT_OK)
+    chirpErrorHandler(err);
+
+ chirp_connect_callback_set_t callback_set = {
+   .on_state_changed = NULL,
+   .on_sending = onSendingCallback,
+   .on_sent = onSentCallback,
+   .on_receiving = NULL,
+   .on_received = NULL
+ };
+
+ err = chirp_connect_set_callbacks(chirp, callback_set);
+ if (err != CHIRP_CONNECT_OK)
+   chirpErrorHandler(err);
+
+  err = chirp_connect_set_output_sample_rate(chirp, SAMPLE_RATE);
+  if (err != CHIRP_CONNECT_OK)
+    chirpErrorHandler(err);
+
+  err = chirp_connect_start(chirp);
+  if (err != CHIRP_CONNECT_OK)
+    chirpErrorHandler(err);
+
+  Serial.println("Chirp SDK initialised.");
+  Serial.flush();
+}
+
+// I2S DMA ---------------------------------------------------
+
+void dmaCallback(Adafruit_ZeroDMA *dma)
+{
+  current++;
+  if (current >= NUM_BUFFERS) {
+    current -= NUM_BUFFERS;
+  }
+  dma->changeDescriptor(desc, buffer[current]);
+  ZeroDMAstatus stat = dma->startJob();
+  if (stat == DMA_STATUS_OK) {
+    dma_complete = true;
+  } else {
+    Serial.println("ERROR");
+  }
+}
+
+void dmaErrorCallback(Adafruit_ZeroDMA *dma)
+{
+  Serial.println("DMAERROR");
+}
+
+void setupDMA(void)
+{
+  Serial.println("Configuring DMA trigger");
+  dma.setTrigger(I2S_DMAC_ID_TX_0);
+  dma.setAction(DMA_TRIGGER_ACTON_BEAT);
+
+  Serial.println("Allocating DMA channel");
+  ZeroDMAstatus stat = dma.allocate();
+  dma.printStatus(stat);
+
+  Serial.println("Setting up transfer");
+  desc = dma.addDescriptor(
+    buffer[current],
+    (void *)(&I2S->DATA[0].reg),
+    BUFFER_SIZE,
+    DMA_BEAT_SIZE_WORD,
+    true,
+    false
+  );
+
+  Serial.println("Adding DMA callbacks");
+  dma.setCallback(dmaCallback);
+  dma.setCallback(dmaErrorCallback, DMA_CALLBACK_TRANSFER_ERROR);
 }
